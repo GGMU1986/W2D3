b185392c66aee95faf2b2e22599c6e8bd8048aa1
gtsimis <gt2186@gmail.com>
2021-08-18 09:42:39 -0400

    one spec off from finished Part 2 mastermind project

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Code
   attr_reader :pegs
 
@@ -38,6 +40,30 @@ class Code
   end
 
   def num_exact_matches(guess)
+    (0...guess.length).count {|i| @pegs[i] == guess[i] }
+  end
+  
+  def num_near_matches(guess)
+    count = 0
 
+    (0...guess.length).each do |i|
+      if guess[i] != @pegs[i] && @pegs.include?(guess[i])
+        count += 1 
+      end
+    end
+    
+    count 
+  end
+
+  def ==(guess)
+    self.pegs == guess.pegs
   end
 end
+
+  # @pegs = ["R", "G", "R", "B"]
+  # guess_1 = ["R", "G", "B", "B"] - 0
+  # guess_2 = ["R", "R", "B", "B"] - 1
+  # guess_3 = ["G", "R", "R", "R"] - 2
+  
+  # p num_near_matches(guess)
+  #new_pegs = ["R", "G", "B"]
