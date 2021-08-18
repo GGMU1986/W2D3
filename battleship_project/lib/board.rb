6bfe73b3e565824930f76d56f94269975ed24455
gtsimis <gt2186@gmail.com>
2021-08-18 14:09:34 -0400

    battleship proj part 1 finished

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,29 @@
 class Board
-  
+    attr_reader :size
+
+  def initialize(n)
+    @grid = Array.new(n) { Array.new(n, :N) }
+    @size = n * n
+  end
+
+  def [](pos)
+    row = pos[0]
+    col = pos[1]
+    @grid[row][col]
+  end
+
+  def []=(pos, val)
+    row = pos[0]
+    col = pos[1]
+    @grid[row][col] = val
+  end
+
+  def num_ships
+    @grid.flatten.count { |ele| ele == :S }
+  end
+
+  def attack
+    
+  end
+ 
 end
